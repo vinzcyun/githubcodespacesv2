4ef36c78c5404db962fda520c5d018b2b839c390
Tự tin code giỏi hơn những thằng tán em <126393227+VinDaiDe@users.noreply.github.com>
2023-09-09 18:24:21 +0700

    Lệnh tạo VPS

--- lenh1.sh
+++ lenh1.sh
@@ -10,4 +10,8 @@ echo "in - Ấn Độ (Mumbai)"
 read -p "Nhập khu vực: " CRP
 ./ngrok tcp --region $CRP 5900 &>/dev/null &
 clear && sudo apt update && sudo apt install qemu-kvm -y && read -p "Nhập dung lượng ổ đĩa: " disk_size
-qemu-img create -f raw win.img "$disk_size" && clear && sudo qemu-system-x86_64 -m 12G -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+qemu-img create -f raw win.img "$disk_size" && clear && sudo qemu-system-x86_64 -m 12G -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin && clear &&
+echo "Thành công"
+echo "Vui lòng lên Ngrok để lấy địa chỉ IP"
+echo "Khu vực dưới này dành cho việc ghi lại báo cáo hoạt động trên RDP"
+echo "-----------------------------------"
