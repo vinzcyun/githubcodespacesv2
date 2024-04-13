f9797b0f0d1d8685248f4213d6dc8f742a9adf1c
duma <126393227+vinzcyun@users.noreply.github.com>
2024-04-13 20:19:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -86,5 +86,5 @@ while true; do
     echo "Thành công!"
     sudo qemu-system-x86_64 -m 12G -cpu host -boot order=c -drive file=link.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
     echo "Lỗi"
-    sleep 2
+    sleep 2 && clear
 done
