cf5958b6cfe40c980fa5b507225489f4a4739241
duma <126393227+vinzcyun@users.noreply.github.com>
2024-04-13 19:54:37 +0700

    Update Command.sh

--- Command.sh
+++ Command.sh
@@ -82,9 +82,6 @@ while true; do
     echo "Đã kết nối"
     echo "Địa chỉ IP truy cập của bạn sẽ vẫn như cũ"
     sudo qemu-system-x86_64 -m 12G -cpu host -boot order=c -drive file=link.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
-    echo "Đang tiếp tục kết nối lại..."
-    echo "Thành công!"
-    sudo qemu-system-x86_64 -m 12G -cpu host -boot order=c -drive file=link.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
-    echo "Lỗi"
+    
     sleep 2
 done
